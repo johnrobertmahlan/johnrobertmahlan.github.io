b8a3de06bc891e59d1d68dbc2d8b55c2b9537a02
John Robert Mahlan <johnrobertmahlan@Johns-Air.jetpack>
2020-05-09 09:55:06 -0600

    clean up functions and event listeners

--- js/script.js
+++ js/script.js
@@ -7,9 +7,8 @@ const baseUrl =  'https://johnrobertmahlan.github.io/'
 
 let userInput; // the user of this app should be able to enter any band to get information about that band
 
-let name, bio;
+let name, bio, bandData;
 
-let aside = false;
 
 // Cached Elements
 
@@ -36,7 +35,7 @@ $('form').on('submit', getBand);
 
 // Functions
 
-//This is the basic function that my app should run: when a band is entered, we access the API to get info about that band
+//This is the basic function that my app runs: a user enters a musician and the app accesses the API for information about that musician
 function getBand(evt) {
     evt.preventDefault();
     
@@ -46,76 +45,89 @@ function getBand(evt) {
     $.ajax({
         url:  'https://ws.audioscrobbler.com/2.0/?method=artist.getinfo&artist=' + userInput +'&api_key=32f924ae8e17d58763e024385d61626d&format=json'
     }).then(function(data) {
-        name = data.artist.name;
-        bio = data.artist.bio.summary;
-        h1El.innerHTML = name;
-        pEl.innerHTML = bio;
-        divEl.innerHTML = '';
-        
-        // Generate a button to allow the user to find artists similar to the one they searched for
-        let firstButtonEl = document.createElement('button');
-        firstButtonEl.textContent = 'Similar Artists'
-        firstButtonEl.addEventListener('click', function() {
-            h3El.textContent = "Similar Artists";
-            ulEl.innerHTML = '';
-
-            for(let i=0; i<data.artist.similar.artist.length; i++) {
-                let newLiElement = document.createElement('li');
-                let similarName = data.artist.similar.artist[i].name;
-                let similarLink = data.artist.similar.artist[i].url;
-                newLiElement.innerHTML = `<a href="${similarLink}" target="_blank">${similarName}</a>`;
-                ulEl.appendChild(newLiElement);
-            }
-        })
-        divEl.appendChild(firstButtonEl);
-
-
-        // Generate a button to allow the user to find the top albums from the artist they searched for
-        let secondButtonEl = document.createElement('button');
-        secondButtonEl.textContent = 'Top Albums'
-        secondButtonEl.addEventListener('click', function() {
-            h3El.textContent = `Top Albums by ${name}`;
-            ulEl.innerHTML = '';
-
-            $.ajax({
-                url: 'https://ws.audioscrobbler.com/2.0/?method=artist.gettopalbums&artist=' + userInput +'&api_key=32f924ae8e17d58763e024385d61626d&format=json'
-            }).then(function(albumData) {
-                for(let j=0; j<albumData.topalbums.album.length && j<5; j++) {
-                    let newLiElement = document.createElement('li');
-                    let topAlbumName = albumData.topalbums.album[j].name;
-                    let topAlbumLink = albumData.topalbums.album[j].url;
-                    newLiElement.innerHTML =  `<a href="${topAlbumLink}" target="_blank">${topAlbumName}</a>`;
-                    ulEl.appendChild(newLiElement);
-                }
-            }, function(error) {
-                console.log(error);
-            });
-        });
-        divEl.appendChild(secondButtonEl);
-
-
-        // Generate a button to allow the user to find the top songs from the artist they searched for
-        let thirdButtonEl = document.createElement('button');
-        thirdButtonEl.textContent = 'Top Songs'
-        thirdButtonEl.addEventListener('click', function() {
-            h3El.textContent = `Top Songs by ${name}`;
-            ulEl.innerHTML = '';
-
-            $.ajax({
-                url: 'https://ws.audioscrobbler.com/2.0/?method=artist.gettoptracks&artist=' + userInput +'&api_key=32f924ae8e17d58763e024385d61626d&format=json'
-            }).then(function(songData)  {
-                for(let k=0; k<songData.toptracks.track.length && k<5; k++) {
-                    let newLiElement = document.createElement('li');
-                    let topSongName = songData.toptracks.track[k].name;
-                    newLiElement.innerHTML = `"${topSongName}"`;
-                    ulEl.appendChild(newLiElement);
-                }
-            }, function(error)  {
-                console.log(error);
-            });
-        });
-        divEl.appendChild(thirdButtonEl);
+        bandData = data; // assign data to a global variable for access in the render function
+        render();
     }, function(error) {
-        console.log(error) 
+        console.log(error);
     })
+};
+
+// When the API is accessed, this function is called - it displays biographical information about the musician the user searched for
+// This function also generates three buttons with event listeners attached so the user can learn more about the artist they selected
+function render() {
+    name = bandData.artist.name;
+    bio = bandData.artist.bio.summary;
+    h1El.innerHTML = name;
+    pEl.innerHTML = bio;
+    divEl.innerHTML = '';
+
+    // Generate a button to allow the user to find artists similar to the one they searched for
+    firstButtonEl = document.createElement('button');
+    firstButtonEl.textContent = 'Similar Artists';
+    firstButtonEl.addEventListener('click', findSimilarArtists);
+    divEl.appendChild(firstButtonEl);
+
+    // Generate a button to allow the user to find the top albums from the artist they searched for
+    secondButtonEl = document.createElement('button');
+    secondButtonEl.textContent = 'Top Albums';
+    secondButtonEl.addEventListener('click', findTopAlbums);
+    divEl.appendChild(secondButtonEl);
+
+    // Generate a button to allow the user to find the top songs from the artist they searched for
+    thirdButtonEl = document.createElement('button');
+    thirdButtonEl.textContent = 'Top Songs';
+    thirdButtonEl.addEventListener('click', findTopSongs);
+    divEl.appendChild(thirdButtonEl);
+};
+
+// This function searches the API database and generates a list of 5 artists similar to the artist the user searched for
+function findSimilarArtists() {
+    h3El.textContent = "Similar Artists";
+    ulEl.innerHTML = '';
+    for(let i=0; i<bandData.artist.similar.artist.length; i++) {
+        let newLiElement = document.createElement('li');
+        let similarName = bandData.artist.similar.artist[i].name;
+        let similarLink = bandData.artist.similar.artist[i].url;
+        newLiElement.innerHTML = `<a href="${similarLink}" target="_blank">${similarName}</a>`;
+        ulEl.appendChild(newLiElement);
+    }
+};
+
+
+// This function runs a new AJAX request to find the top 5 albums in the API's database associated with the artist entered by the user
+function findTopAlbums() {
+    h3El.textContent = `Top Albums by ${name}`;
+    ulEl.innerHTML = '';
+    $.ajax({
+        url: 'https://ws.audioscrobbler.com/2.0/?method=artist.gettopalbums&artist=' + userInput +'&api_key=32f924ae8e17d58763e024385d61626d&format=json'
+    }).then(function(albumData) {
+        for(let j=0; j<albumData.topalbums.album.length && j<5; j++) {
+            let newLiElement = document.createElement('li');
+            let topAlbumName = albumData.topalbums.album[j].name;
+            let topAlbumLink = albumData.topalbums.album[j].url;
+            newLiElement.innerHTML =  `<a href="${topAlbumLink}" target="_blank">${topAlbumName}</a>`;
+            ulEl.appendChild(newLiElement);
+        }
+    }, function(error) {
+        console.log(error);
+    });
+};
+
+
+// This function runs a new AJAX request to find the top 5 songs in the API's database associated with the artist entered by the user
+function findTopSongs() {
+    h3El.textContent = `Top Songs by ${name}`;
+    ulEl.innerHTML = '';
+    $.ajax({
+        url: 'https://ws.audioscrobbler.com/2.0/?method=artist.gettoptracks&artist=' + userInput +'&api_key=32f924ae8e17d58763e024385d61626d&format=json'
+    }).then(function(songData)  {
+        for(let k=0; k<songData.toptracks.track.length && k<5; k++) {
+            let newLiElement = document.createElement('li');
+            let topSongName = songData.toptracks.track[k].name;
+            newLiElement.innerHTML = `"${topSongName}"`;
+            ulEl.appendChild(newLiElement);
+        }
+    }, function(error)  {
+        console.log(error);
+    });
 };
